bd0d203c956aa76ad2d6393b56f0425211688e32
marco <marcodecrema@libero.it>
2020-09-02 17:18:27 +0200

    pallini blu

--- js/script.js
+++ js/script.js
@@ -5,20 +5,27 @@ $(document).ready(function() {
   var right = $(".next");
   var imgFirst = $(".first");
   var imgLast = $(".last");
+  var puntoFirst = $(".first");
   var counter = 0;
-
 // Evento click sulla freccina destra del carousel
   right.click(function() {
     var img = $("img.active");
     img.removeClass("active");
-    if (img.hasClass("last")) {
+    var punto = $("i.active");
+    punto.removeClass("active");
+
+    if (img.hasClass("last") && punto.hasClass("last")) {
     var imgNext = imgFirst;
+    var puntoNext = puntoFirst;
   } else {
     var imgNext = img.next();
+    var puntoNext = punto.next();
   }
   imgNext.addClass("active");
+  puntoNext.addClass("active");
   });
 
+// Evento click sulla freccina sinistra del carousel
   left.click(function() {
     var img = $("img.active");
     img.removeClass("active");
@@ -29,5 +36,7 @@ $(document).ready(function() {
   }
   imgNext.addClass("active");
   });
+
+
 
 });
