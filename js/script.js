2c60af881db0f996ca99df330c4ef68b61966020
marco <marcodecrema@libero.it>
2020-09-02 17:27:31 +0200

    commenti

--- js/script.js
+++ js/script.js
@@ -5,7 +5,6 @@ $(document).ready(function() {
   var right = $(".next");
   var imgFirst = $(".first");
   var imgLast = $(".last");
-  var puntoFirst = $(".first");
   var counter = 0;
 // Evento click sulla freccina destra del carousel
   right.click(function() {
@@ -14,29 +13,40 @@ $(document).ready(function() {
     var punto = $("i.active");
     punto.removeClass("active");
 
+    // Se l'img ha classe 'last' e il pallino ha classe 'last'
     if (img.hasClass("last") && punto.hasClass("last")) {
+      // Allora significa che cliccando a destra dovrò vedere la prima immagine e il primo pallino sarà blu
     var imgNext = imgFirst;
-    var puntoNext = puntoFirst;
+    var puntoNext = imgFirst;
   } else {
+    // Altrimenti verrà selezionata l'immagine successiva a quella che al momento a classe 'active'
     var imgNext = img.next();
+    // Stesso cosa per il pallino
     var puntoNext = punto.next();
   }
+
+  // Una volta individuato l'elemento corretto gli aggiungo la classe 'active' in modo da renderlo visibile
   imgNext.addClass("active");
   puntoNext.addClass("active");
   });
 
 // Evento click sulla freccina sinistra del carousel
+// Stesso cosa della freccina destra però col ragionamento inverso
   left.click(function() {
     var img = $("img.active");
     img.removeClass("active");
-    if (img.hasClass("first")) {
+    var punto = $("i.active");
+    punto.removeClass("active");
+
+    if (img.hasClass("first") && punto.hasClass("first")) {
     var imgNext = imgLast;
+    var puntoNext = imgLast;
   } else {
     var imgNext = img.prev();
+    var puntoNext = punto.prev();
   }
   imgNext.addClass("active");
+  puntoNext.addClass("active");
   });
-
-
 
 });
