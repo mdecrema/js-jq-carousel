50822357aeb7c10e44731331398ef9ab1d91edba
marco <marcodecrema@libero.it>
2020-09-02 17:03:22 +0200

    prova 2

--- js/script.js
+++ js/script.js
@@ -9,43 +9,15 @@ $(document).ready(function() {
 
 // Evento click sulla freccina destra del carousel
   right.click(function() {
-    if (counter == 0) {
-    imgFirst.removeClass("active");
-    imgFirst.next().addClass("active");
-    counter = 1;
-  } else if ( counter == 1) {
-      imgFirst.next().removeClass("active");
-      imgLast.prev().addClass("active");
-      counter = 2;
-  } else if ( counter == 2) {
-    imgLast.prev().removeClass("active");
-    imgLast.addClass("active");
-    counter = 3;
-  } else if ( counter == 3){
-    imgLast.removeClass("active");
-    imgFirst.addClass("active");
-    counter = 0;
+    var img = $("img.active");
+    img.removeClass("active");
+    if (img.hasClass("last")) {
+    var imgNext = imgFirst;
+  } else {
+    var imgNext = img.next();
   }
+  imgNext.addClass("active");
   });
-
-left.click(function() {
-  if (counter == 0) {
-  imgLast.addClass("active");
-  imgFirst.removeClass("active");
-  counter = 3;
-} else if ( counter == 3) {
-    imgLast.prev().addClass("active");
-    imgLast.removeClass("active");
-    counter = 2;
-} else if ( counter == 2) {
-  imgLast.prev().removeClass("active");
-  imgFirst.next().addClass("active");
-  counter = 1;
-} else if ( counter == 1) {
-  imgFirst.next().removeClass("active");
-  imgFirst.addClass("active");
-}
-});
 
 
 });
